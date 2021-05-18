cfe730e4a134e1ccc085cc814bdb2a51151c52e9
Steven Dunn <Dunn.Steven1@gmail.com>
2021-05-17 19:26:18 -0700

    Refactored the CardList component to be a functional component to test using functional and class components together. Added hard-coded data for testing.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './app.css';
 import React from 'react';
 
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.title = 'Yo';
-  }
-
   render() {
     return (
       <div className="header">{this.props.title}</div>,
@@ -18,27 +20,26 @@ class App extends React.Component {
 
 class Card extends React.Component {
   render() {
+    const profile = testData[0];
     return (
       <div className="github-profile">
-        <img alt="" src="https://placehold.it/75" />
+        <img alt="" src={profile.avatar_url} />
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     );
   }
 }
 
-class CardList extends React.Component {
-  render() {
-    return (
-      <div className="card-list">
-        <Card/>
-        <Card/>
-      </div>
-    );
-  }
+const CardList = (props) => {
+  return (
+    <div className="card-list">
+      <Card/>
+      <Card/>
+    </div>
+  );
 }
 
 export default App;
