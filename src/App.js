c009597eb19ee7aa984d9ff20f14887018016fa5
Steven Dunn <Dunn.Steven1@gmail.com>
2021-05-17 20:58:14 -0700

    Removed the constructor since it's not actually needed. You can just create the state object directly as a property on the Class.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './app.css';
 import React from 'react';
 
-
 const testData = [
     {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
     {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
     {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
-
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      profileData: testData
-    };
-  }
+  state = {
+    // The state MUST be an object in a class component. It can't be a string or normal like with a functional component.
+    profileData: testData
+  };
 
   render() {
     return (
